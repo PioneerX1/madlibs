fcd2b413f8db7bedd235e99ce555b43eb3622a0f
lincoln nguyen and Mick Sexton <mick@>
2017-06-05 10:20:19 -0700

    replace js variables with a loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
   $("#formOne").submit(function() {
+    var answers = ["person", "animal", "place"];
+
+    answers.forEach(function(answer) {
+      //answers.push($("input#" + answer).val());
+      var userInput = $("input#" + answer).val();
+      $("." + answer).text(userInput);
+    });
+
+    /*
     var personInput = $("input#person").val();
     var animalInput = $("input#animal").val();
     var placeInput = $("input#place").val();
@@ -7,6 +16,8 @@ $(document).ready(function() {
     $(".person").text(personInput);
     $(".animal").text(animalInput);
     $(".place").text(placeInput);
+
+    */
 
     $("#story").show();
 
